9f4beb4778a9c6695d41970e5c61dfbeb5c921e3
tatiana <dt.guatapo@unimayor.edu.co>
2025-08-26 16:49:57 -0500

    clase de 26

---
+++ 01-bases-react/src/components/BasicTypes.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const BasicTypes= () => {
+
+    const name: string = "TATIANA";
+   const age: number = 20;
+   let isActive: boolean = true;
+
+  return (
+    <>
+      <h3>Basic Types</h3>
+      <ul>
+        <div>TATI (●'◡'●)</div>
+        <li>Nombre: {name}</li>
+        <li>Edad: {age}</li>
+
+
+        
+        <></>
+      </ul>
+    </>
+)
+
+}
+
