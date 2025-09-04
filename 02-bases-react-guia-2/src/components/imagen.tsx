8bade05697fd63e78b5fb7f3e4c1161853166a6e
tatiana} <dt.guatapo@unimayor.edu.co>
2025-09-04 18:20:42 -0500

    probar el commit

---
+++ 02-bases-react-guia-2/src/components/imagen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const imagen = () => {
+  return (
+    <>
+      <ul>hola </ul>
+      <h2>como </h2>
+      <h4>holaaa</h4> 
+    </>
+  )
+}
+
+export default imagen
